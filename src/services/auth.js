df53ebe44f8ed706d1f5e3ed0c57d03c9c53f7d7
joseandre <contretaa@gmail.com>
2022-02-22 11:47:13 -0400

    add: set local env
    Signed-off-by: joseandre <contretaa@gmail.com>

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:4000';
+const baseUrl =
+  process.env.NODE === 'develop'
+    ? process.env.REACT_APP_LOCAL_BACKEND_URL
+    : REACT_APP_BACKEND_URL;
 
 export const sigin = (body = {}) =>
   axios.post(`${baseUrl}/api/auth/signIn`, body);
--- src/services/infoPage.js
+++ src/services/infoPage.js
@@ -1,6 +1,6 @@
 import { createInstance } from '../config/axios.config';
 
-const apiToken = '4f0045a8-f138-4c52-8c20-d1fd137a5837';
+const apiToken = process.env.REACT_APP_API_TOKEN;
 
 const baseUrl = 'https://api.peekalink.io/';
 
